5cec1b74c7239c56fac80b9d4cf41340d0e39173
Cody <stammer.3@osu.edu>
2017-12-17 16:38:00 -0500

    Routing project

---
+++ routing/src/app/app.routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent},
+    ]},
+    {path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
+    ]},
+    // {path: 'not-found', component: PageNotFoundComponent},
+    {path: 'not-found', component: ErrorPageComponent,  data: {message: 'Page not found!'}},
+    {path: '**', redirectTo: '/not-found'}
+    ];
+
+@NgModule({
+    imports: [
+        // this is for servers that do not return index.html when 404
+        // RouterModule.forRoot(appRoutes, {useHash: true})
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
+
